395e320dab9cc8b6d228d3f7aa585d8b77788f7e
Дмитрий Мельников <mooneez.mnz@gmail.com>
2024-02-07 10:57:33 +0300

    Окончательный рефакторинг + добавление условий проверки

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from dict import *
 import random
 import os
+import re
 
 clear = lambda: os.system('cls') 
+
 
 def get_random_word():
     return random.choice(dictionary)
@@ -11,7 +13,7 @@ def check_inputed_letter(word_arr, inputed_letter):
     return [i for i, ltr in enumerate(word_arr) if ltr == inputed_letter]
 
 def check_char(inputed_letter):
-    return inputed_letter.isalpha() and len(inputed_letter) == 1 and not inputed_letter.isdigit()
+    return inputed_letter.isalpha() and len(inputed_letter) == 1 and not inputed_letter.isdigit() and bool(re.search('[а-яА-Я]', inputed_letter)) == True
 
 def play_again():
     ask = input("Хотите ли вы начать игру вновь? [да/нет]:").lower()
@@ -23,6 +25,7 @@ def gallows_loop():
     word = get_random_word()
     word_arr = list(word)
     guess = list("_"*len(word))
+    inputed_letters = []
     
     attempt_counter = 0
     if len(word)>=7:
@@ -35,30 +38,35 @@ def gallows_loop():
         print ("Осталось попыток: ", attempt_counter, "\n")
         inputed_letter=(input("Введите букву - ")).lower()
         if check_char(inputed_letter):
-            if inputed_letter in word_arr:
-                clear()
-                x = check_inputed_letter(word_arr, inputed_letter)
-                
-                for j in x: 
-                    guess[j] = inputed_letter
-                print("".join(guess))
+            if inputed_letter not in inputed_letters:
+                if inputed_letter in word_arr:
+                    clear()
+                    inputed_letters+=inputed_letter
+                    x = check_inputed_letter(word_arr, inputed_letter)
+
+                    for j in x: 
+                        guess[j] = inputed_letter
+                    print("".join(guess))
+                else:
+                    clear()
+                    inputed_letters+=inputed_letter
+                    print("".join(guess))
+                    attempt_counter-=1
+                    print ("Не угадал")
+                    if attempt_counter < 5: print ("  | ")
+                    if attempt_counter < 4: print ("  O ")
+                    if attempt_counter < 3: print (" /|\ ")
+                    if attempt_counter < 2: print ("  | ")
+                    if attempt_counter < 1: print (" / \ ")
+                    if attempt_counter == 0:
+                        print("К сожалению, тебя повесили.")
+                        if play_again():
+                            gallows_loop()
+                        else:
+                            clear()
+                            exit()
             else:
-                clear()
-                print("".join(guess))
-                attempt_counter-=1
-                print ("Не угадал")
-                if attempt_counter < 5: print ("  | ")
-                if attempt_counter < 4: print ("  O ")
-                if attempt_counter < 3: print (" /|\ ")
-                if attempt_counter < 2: print ("  | ")
-                if attempt_counter < 1: print (" / \ ")
-                if attempt_counter == 0:
-                    print("К сожалению, тебя повесили.")
-                    if play_again():
-                        gallows_loop()
-                    else:
-                        clear()
-                        exit()
+                print("Данный символ уже был введен")
         else:
             print("Некорректный ввод")
             
